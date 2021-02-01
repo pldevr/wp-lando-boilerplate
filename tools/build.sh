986f723794a7fc5b712726d7a8e679d175bd9994
Tomasz Rusiecki <rusiecki.tom@gmail.com>
2021-02-01 23:30:51 +0100

    appserver build scripts

---
+++ tools/build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if [ -z $BUILD_WP_THEME_NAME ]; then
+  echo "build.sh # Set environment: BUILD_WP_THEME_NAME";
+  exit 1;
+fi
+
+# Bedrock
+composer install --no-interaction --working-dir=/app/web
+rm -rf /app/web/web/wp/wp-content
+
+# Sage
+composer install --no-interaction --working-dir=/app/web/web/app/themes/${BUILD_WP_THEME_NAME}
