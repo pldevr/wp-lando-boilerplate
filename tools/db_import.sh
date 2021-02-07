70da91d1f8a86dc1973b4ff74fc6fd38d1edb871
Tomasz Rusiecki <rusiecki.tom@gmail.com>
2021-02-07 13:47:59 +0100

    db file does not exist

--- tools/db_import.sh
+++ tools/db_import.sh
@@ -2,6 +2,11 @@
 
 path=/app/web/web/wp
 
+if [ ! -f /app/db/db.sql ]; then
+  echo "db_import.sh # DB file does not exist";
+  exit 1;
+fi
+
 echo 'y' | wp db reset --path=${path}
 wp db import /app/db/db.sql --path=${path}
 # wp search-replace 'FROM' 'TO' --precise --all-tables-with-prefix --path=${path}
