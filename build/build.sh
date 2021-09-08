ef90da31a302e960179ea6a44f2a1769f2780fad
Tomasz Rusiecki <rusiecki.tom@gmail.com>
2021-09-08 12:15:06 +0200

    comment theme build

--- build/build.sh
+++ build/build.sh
@@ -2,17 +2,14 @@
 
 . ./config.sh
 
-if [ -z $BUILD_WP_THEME_NAME ]; then
-  echo "build.sh # Set environment: BUILD_WP_THEME_NAME"
-  exit 1
-fi
-
 # Bedrock
 composer install --no-interaction --working-dir=${BUILD_PATH}/web
 rm -rf ${BUILD_PATH}/web/web/wp/wp-content
 
-# Sage
-composer install --no-interaction --working-dir=${BUILD_PATH}/web/web/app/themes/${BUILD_WP_THEME_NAME}
+#if [ -z $BUILD_WP_THEME_NAME ]; then
+#  echo "build.sh # required: BUILD_WP_THEME_NAME"
+#  exit 1
+#fi
 
-# DB
-sh ${BUILD_PATH}/tools/db_import.sh
+# Theme
+#composer install --no-interaction --working-dir=${BUILD_PATH}/web/web/app/themes/${BUILD_WP_THEME_NAME}
--- build/build_node.sh
+++ build/build_node.sh
@@ -2,10 +2,10 @@
 
 . ./config.sh
 
-if [ -z $BUILD_WP_THEME_NAME ]; then
-  echo "build_node.sh # Set environment: BUILD_WP_THEME_NAME"
-  exit 1
-fi
+# if [ -z $BUILD_WP_THEME_NAME ]; then
+#   echo "build_node.sh # required: BUILD_WP_THEME_NAME"
+#   exit 1
+# fi
 
-# Sage
-cd ${BUILD_PATH}/web/web/app/themes/${BUILD_WP_THEME_NAME} && npm i && npm run build:production
+# Theme
+# cd ${BUILD_PATH}/web/web/app/themes/${BUILD_WP_THEME_NAME} && npm i && npm run build:production
