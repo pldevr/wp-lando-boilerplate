133833d59ff45ea319c32b8dcd072c9a228b4991
Tomasz Rusiecki <rusiecki.tom@gmail.com>
2021-09-12 01:38:49 +0200

    add config to db_import tool & remove wp search-replace

--- build/db_import.sh
+++ build/db_import.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
+. $(dirname $(realpath $0))/config.sh
+
 if [ ! -f ${BUILD_PATH}/db/db.sql ]; then
   echo "db_import.sh # DB file does not exist"
   exit 1
 fi
 
-echo 'y' | wp db reset
+wp db reset --yes
 wp db import ${BUILD_PATH}/db/db.sql
-# wp search-replace 'FROM' 'TO' --precise --all-tables-with-prefix
